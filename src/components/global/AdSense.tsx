b719ad9399afbea87e53d5bf000e012ddb8a6ae0
Robin <robindev2007@gmail.com>
2025-12-13 08:49:10 +0600

    added robot.ts

--- src/components/global/AdSense.tsx
+++ src/components/global/AdSense.tsx
@@ -6,6 +6,7 @@ const AdSense = () => {
       async
       src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4524794510936601`}
       crossOrigin="anonymous"
+      strategy="afterInteractive"
     />
   );
 };
